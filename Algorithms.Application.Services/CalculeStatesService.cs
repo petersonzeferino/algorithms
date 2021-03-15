4d95f3595c5e050a2ebc0dd76a4b647a642f6d32
Peterson Zeferino <petersonzeferino@hotmail.com>
2021-03-14 23:24:37 -0300

    Refactoring - Architecture CalculeStates, NumberOfPairs and TopCompetitors

---
+++ Algorithms.Application.Services/CalculeStatesService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Application.Services
+{
+    public class CalculeStatesService : ICalculeStatesService
+    {
+
+    }
+}
---
+++ Algorithms.Application.Services/NumberOfPairsService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Application.Services
+{
+    public class NumberOfPairsService : INumberOfPairsService
+    {
+    }
+}
