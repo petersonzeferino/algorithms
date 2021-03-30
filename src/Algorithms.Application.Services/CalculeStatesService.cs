4a8c1fccaca3a07a1dd25163427d004b31aff126
Peterson <petersonzeferino@hotmail.com>
2021-03-30 16:20:03 -0300

    Refactoring Services Layer

--- src/Algorithms.Application.Services/CalculeStatesService.cs
+++ src/Algorithms.Application.Services/CalculeStatesService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Algorithms.Application.Services
+﻿namespace Algorithms.Application.Services
 {
     public class CalculeStatesService : ICalculeStatesService
     {
