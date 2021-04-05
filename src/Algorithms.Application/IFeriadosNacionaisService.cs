1cfef02a57ddc82cb867eb225c5b76407343cdd6
Peterson <petersonzeferino@hotmail.com>
2021-04-05 16:41:18 -0300

    Include algorithm to valid Feriados Nacionais

---
+++ src/Algorithms.Application/IFeriadosNacionaisService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Application
+{
+    public interface IFeriadosNacionaisService
+    {
+        public DateTime ObterDataValida(DateTime dataConsiderada);
+    }
+}
