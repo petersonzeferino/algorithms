f774eb61327f76b309b7d6f777aaee96d449efef
Peterson <petersonzeferino@hotmail.com>
2021-04-24 11:29:38 -0300

    Remove Test Feriados Nacionais

--- src/AlgorithmsTest/Tests/FeriadosNacionaisTest.cs
+++ src/AlgorithmsTest/Tests/FeriadosNacionaisTest.cs
@@ -7,18 +7,18 @@ namespace AlgorithmsTest.Tests
 {
     public class FeriadosNacionaisTest
     {
-        [Theory(DisplayName = "Check valid date")]
-        [InlineData("05/04/2021", "06/04/2021")]
-        [InlineData("10/04/2021", "12/04/2021")]
-        [InlineData("16/04/2021", "19/04/2021")]
-        public void CheckObterDataValidaSuccess(string inputString, string outputString)
-        {
-            IFeriadosNacionaisService _feriadosNacionaisService  = new FeriadosNacionaisService();
+        //    [Theory(DisplayName = "Check valid date")]
+        //    [InlineData("05/04/2021", "06/04/2021")]
+        //    [InlineData("10/04/2021", "12/04/2021")]
+        //    [InlineData("16/04/2021", "19/04/2021")]
+        //    public void CheckObterDataValidaSuccess(string inputString, string outputString)
+        //    {
+        //        IFeriadosNacionaisService _feriadosNacionaisService  = new FeriadosNacionaisService();
 
-            DateTime input = Convert.ToDateTime(inputString); 
-            DateTime output = Convert.ToDateTime(outputString);
+        //        DateTime input = Convert.ToDateTime(inputString); 
+        //        DateTime output = Convert.ToDateTime(outputString);
 
-            Assert.Equal(_feriadosNacionaisService.ObterDataValida(input), output);
-        }
+        //        Assert.Equal(_feriadosNacionaisService.ObterDataValida(input), output);
+        //    }
     }
 }
