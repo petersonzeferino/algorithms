7c9572e4293e80d92a2be215ced4206c58ec76b1
Peterson <petersonzeferino@hotmail.com>
2021-11-17 23:05:40 -0300

    Create Class and Inteface to PlusMinus

---
+++ src/Algorithms.Application.Services/PlusMinus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Application.Services
+{
+    public class PlusMinus : IPlusMinus
+    {
+        public List<string> GetPlusMinus(List<int> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Algorithms.Application/IPlusMinus.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Application
+{
+    public interface IPlusMinus
+    {
+        List<string> GetPlusMinus(List<int> list);
+    }
+}
