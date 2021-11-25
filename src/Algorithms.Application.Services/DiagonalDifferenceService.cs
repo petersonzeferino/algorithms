f1db4e8cac1fcb008928a229fe7258df78ab28d9
Peterson <petersonzeferino@hotmail.com>
2021-11-24 21:35:22 -0300

    Solved DiagonalDifference

--- src/Algorithms.Application.Services/DiagonalDifferenceService.cs
+++ src/Algorithms.Application.Services/DiagonalDifferenceService.cs
@@ -7,17 +7,13 @@ namespace Algorithms.Application.Services
     {
         public int CalculeDiagonalDifference(List<List<int>> arr)
         {
-            //arr = new List<List<int>> { new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }, new List<int> { 9, 8, 9 } };
-            //arr = new List<List<int>> { new List<int> { 11, 2, 4 }, new List<int> { 4, 5, 6 }, new List<int> { 10, 8, -12 } };
-            //arr = new List<List<int>> { new List<int> { -1, 1, -7, -8 }, new List<int> { -10, -8, -5, -2 }, new List<int> { 0, 9, 7, -1 }, new List<int> { 4, 4, -2, 1 } };
-
             int diagonal = 0;
             int inverseDiagonal = 0;
             int capacity = arr.Count - 1;
 
             while (capacity != -1)
             {
-                int current = arr[capacity][capacity];
+                var current = arr[capacity][capacity];
                 diagonal += current;
                 capacity--;
             }
@@ -27,7 +23,7 @@ namespace Algorithms.Application.Services
 
             while (capacity != -1)
             {
-                int current = arr[start][capacity];
+                var current = arr[start][capacity];
                 inverseDiagonal += current;
                 capacity--;
                 start++;
@@ -35,11 +31,11 @@ namespace Algorithms.Application.Services
 
             int result;
             if (diagonal > inverseDiagonal)
-                result = Math.Abs(diagonal - inverseDiagonal);
+                result = diagonal - inverseDiagonal;
             else
-                result = Math.Abs(inverseDiagonal - diagonal);
+                result = inverseDiagonal - diagonal;
 
-            return result;
+            return Math.Abs(result);
         }
     }
 }
--- src/AlgorithmsTest/Tests/DiagonalDifferenceTest.cs
+++ src/AlgorithmsTest/Tests/DiagonalDifferenceTest.cs
@@ -2,6 +2,7 @@
 using Algorithms.Application.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace AlgorithmsTest.Tests
@@ -9,45 +10,35 @@ namespace AlgorithmsTest.Tests
 
     public class DiagonalDifferenceTest
     {
-        [Fact(DisplayName = "DiagonalDifference")]
-        //[InlineData("1,2,3", "0.500000,0.333333,0.166667")]
-        //[InlineData("1,2,3,-1,-2,-3,0,0", "0.375000,0.375000,0.250000")]
-        public void DiagonalDifferenceTestWithSuccess()
+        [Theory(DisplayName = "DiagonalDifference")]
+        [MemberData(nameof(GetParameters), parameters: 3)]
+        public void DiagonalDifferenceTestWithSuccess(List<List<int>> arr, int expected)
         {
+            IDiagonalDifferenceService diagonalDifferenceService = new DiagonalDifferenceService();
+            var result = diagonalDifferenceService.CalculeDiagonalDifference(arr);
 
-            //List<List<int>> arr = new List<List<int>> { new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }, new List<int> { 9, 8, 9 } };
-            List<List<int>> arr = new List<List<int>> { new List<int> { 11, 2, 4 }, new List<int> { 4, 5, 6 }, new List<int> { 10, 8, -12 } };
-            //List<List<int>> arr = new List<List<int>> { new List<int> { -1, 1, -7, -8 }, new List<int> { -10, -8, -5, -2 }, new List<int> { 0, 9, 7, -1 }, new List<int> { 4, 4, -2, 1 } };
+            Assert.Equal(result, expected);
+        }
 
-            int diagonal = 0;
-            int inverseDiagonal = 0;
-            int capacity = arr.Count - 1;
-            
-            while (capacity != -1)
+        public static IEnumerable<object[]> GetParameters(int numberTest)
+        {
+            var allParams = new List<object[]>
             {
-                var current = arr[capacity][capacity];
-                diagonal += arr[capacity][capacity];
-                capacity--;
-            }
+                new object[] {
+                    new List<List<int>> { new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }, new List<int> { 9, 8, 9 } },
+                    2
+                },
+                new object[] {
+                    new List<List<int>> { new List<int> { 11, 2, 4 }, new List<int> { 4, 5, 6 }, new List<int> { 10, 8, -12 } },
+                    15
+                },
+                new object[] {
+                    new List<List<int>> { new List<int> { -1, 1, -7, -8 }, new List<int> { -10, -8, -5, -2 }, new List<int> { 0, 9, 7, -1 }, new List<int> { 4, 4, -2, 1 } },
+                    1
+                },
+            };
 
-            capacity = arr.Count - 1;
-            int start = 0;
-
-            while (capacity != -1)
-            {
-                var current = arr[start][capacity];
-                inverseDiagonal += arr[start][capacity];
-                capacity--;
-                start++;
-            }
-
-            int result = 0;
-            if (diagonal > inverseDiagonal)
-                result = Math.Abs(diagonal - inverseDiagonal);
-            else
-                result = Math.Abs(inverseDiagonal - diagonal);
-
-            //Assert.True(isEqual);            
+            return allParams.Take(numberTest);
         }
     }
 }
