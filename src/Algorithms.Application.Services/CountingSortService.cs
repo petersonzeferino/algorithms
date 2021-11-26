3b58d0f2c504aae689a9c80ba15a1e1baf9bb1dd
Peterson <petersonzeferino@hotmail.com>
2021-11-26 15:44:22 -0300

    Create layers counting sort

---
+++ src/Algorithms.Application.Services/CountingSortService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Application.Services
+{
+    public class CountingSortService : ICountingSortService
+    {
+        public List<int> GetCountingSort(List<int> arr, int numberAlgorithm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Algorithms.Application/ICountingSortService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Application
+{
+    public interface ICountingSortService
+    {
+        List<int> GetCountingSort(List<int> arr);
+    }
+}
