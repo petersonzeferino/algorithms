f6670e43e0f132a673fe4958d312a22a608f6a61
Peterson <petersonzeferino@hotmail.com>
2021-11-26 17:04:59 -0300

    build options in for getCountingSort

--- src/Algorithms.Application.Services/CountingSortService.cs
+++ src/Algorithms.Application.Services/CountingSortService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Algorithms.Application.Services
 {
@@ -7,7 +8,45 @@ namespace Algorithms.Application.Services
     {
         public List<int> GetCountingSort(List<int> arr, int numberAlgorithm)
         {
-            throw new NotImplementedException();
+            return numberAlgorithm switch
+            {
+                1 => SolutiontOne(arr),
+                2 => SolutiontTwo(arr),
+                _ => SolutiontOne(arr)
+            };
+        }
+
+        private List<int> SolutiontOne(List<int> arr)
+        {
+            List<int> result = new List<int>();
+
+            //int max = arr.Max();
+            //for (int i = 0; i < max; i++)
+            //    result.Add(0);
+
+            for (int i = 0; i < 100; i++)
+                result.Add(0);
+
+            foreach (var item in arr)
+            {
+                var index = item - 1;
+
+                if (index < 0)
+                    result[0] += 1;
+                else
+                    result[item] += 1;
+            }
+
+            return result;
+        }
+
+        private List<int> SolutiontTwo(List<int> arr)
+        {
+            int[] result = new int[100];
+            for (int i = 0; i < arr.Count(); i++)
+                result[arr[i]]++;
+
+            return result.ToList();
         }
     }
 }
