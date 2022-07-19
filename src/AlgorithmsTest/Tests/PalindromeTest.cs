3029cfa78a5fdea66f6383011d8d3fc72b42d0cd
Peterson <petersonzeferino@hotmail.com>
2022-07-19 18:26:50 -0300

    (algorithms)! palindrome with letters

--- src/AlgorithmsTest/Tests/PalindromeTest.cs
+++ src/AlgorithmsTest/Tests/PalindromeTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace AlgorithmsTest.Tests
@@ -19,45 +20,16 @@ namespace AlgorithmsTest.Tests
         [InlineData("A car, a man, a maraca", true)]
         public void CheckPalindromeWithSuccess(string input, bool output)
         {
-            Assert.Equal(CheckPalindrome(input), output);
+            Assert.Equal(CheckPalindrome(input.ToLower().Replace(" ", "").Replace(",", "")), output);
         }
 
 
         public bool CheckPalindrome(string param)
         {
-            
-            string text = param.ToLower().Replace(" ", "").Replace(".", "").Replace(",", "").Replace(";", "");
-            string expression = @"^[a-z0-9]";
+            var reverseChar = param.ToCharArray().Reverse();
+            string reverse = new string(reverseChar.ToArray()).ToLower().Replace(" ","").Replace(",", "");
 
-            Regex r = new Regex(expression, RegexOptions.IgnoreCase);
-            Match m = r.Match(param);
-
-            if (m.Success)
-            {
-                if (text.Length == 1)
-                    return true;
-
-                //int i = 0;
-                //int j = text.Length;
-
-                //while (i != j)
-                //{
-                //    if (text.Substring(i, 1) != text.Substring(j - 1, 1))
-                //        return Z = false;                       
-
-                //    i++;
-                //    j--;
-                //}
-
-                //return Z = true;
-                else if (text.Substring(0, 1) == text.Substring(text.Length - 1, 1))
-                    return true;
-
-                else
-                    return false;
-            }
-            else
-                return false;
+            return param.Equals(reverse);
         }
     }
 }
