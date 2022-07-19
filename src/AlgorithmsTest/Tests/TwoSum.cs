03717651ba2b0689f3dfc909a79eaed0f2312df5
Peterson <petersonzeferino@hotmail.com>
2022-07-19 20:24:56 -0300

    (algorithms)! Two Sums Sorted List

--- src/AlgorithmsTest/Tests/TwoSum.cs
+++ src/AlgorithmsTest/Tests/TwoSum.cs
@@ -39,5 +39,46 @@ namespace AlgorithmsTest.Tests
 
             return default;
         }
+
+        /// <summary>
+        /// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+        /// Return the indices of the two numbers, index1 and index2, added by one as an integer array[index1, index2] of length 2.
+        /// The tests are generated such that there is exactly one solution.You may not use the same element twice.
+        /// Your solution must use only constant extra space.
+        /// </summary>
+        [Fact]
+        public void ShouldBeTwoSumAndReturnIndexsInInputArrayIsSorted()
+        {
+            int[] numbers = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] expected = { 1, 2 };
+
+            var result = GetTwoSumSorted(numbers, target);
+
+            Assert.Equal(expected, result);
+        }
+
+        private int[] GetTwoSumSorted(int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+
+            do
+            {
+                int sum = numbers[left] + numbers[right];
+                
+                if(sum == target)
+                    return new int[] { left + 1 , right + 1 };
+
+                if (sum > target)
+                    right--;
+
+                else
+                    left++;
+
+            } while (true);
+
+            return default;
+        }
     }
 }
